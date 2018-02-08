1481d9654f5fad8cb14bcc3a4285309d3c8f2d59
doc Brown <md1guy@live.com>
2018-02-09 01:10:37 +0200

    Added homepage

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const http = require('http');
+
 const public = require('./routes/public');
+const home = require('./routes/home');
 
 http.createServer((req, res) => {
-  public(req, res);
+  if (req.url.match(/.(html|css|js|png)$/)) {
+    public(req, res);
+  } else if (req.url === '/') {
+    home(req, res);
+  } else {
+
+  }
 }).listen(8080, () => console.log('Server is working now'));
